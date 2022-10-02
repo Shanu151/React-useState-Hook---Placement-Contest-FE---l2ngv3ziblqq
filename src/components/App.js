2520b2e9534256df78224521ac664e8ebcfe6685
Shanu151 <77886773+Shanu151@users.noreply.github.com>
2022-10-02 16:15:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 
 import '../styles/App.css';
+
 import React, { useState } from 'react';
 
 export default function App() {
  //code here 
-
- 
+ const [inputValue,setInput]=useState("")
+ const [text,setText]=useState("")
+ const changeInput=(e)=>{
+   setInput(e.target.value);
+ }
+ const buttonClick=()=>{
+  setText((prev)=>prev+inputValue)
+   setInput('')
+ } 
   return (
     <div>
-  <input id='input' value={inputValue} onChange={changeInput} />
-  
+  <input type="text" id='input' value={inputValue} onChange={(e)=>changeInput(e)} />
       <p id='intro'>Concated String</p>
       <p id='text'>{text} </p>
       <button id='button' onClick={buttonClick}>
@@ -18,3 +25,21 @@ export default function App() {
     </div>
   );
 }
+// import React, { useState } from 'react';
+
+// export default function App() {
+//  //code here 
+
+ 
+//   return (
+//     <div>
+//   <input id='input' value={inputValue} onChange={changeInput} />
+  
+//       <p id='intro'>Concated String</p>
+//       <p id='text'>{text} </p>
+//       <button id='button' onClick={buttonClick}>
+//         Click me
+//       </button>
+//     </div>
+//   );
+// }
